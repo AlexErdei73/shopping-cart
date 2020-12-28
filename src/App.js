08ec63c9cd99f99c28654ea4a7c6155b877e92a4
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-12-28 11:08:27 +0000

    add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Home from "./components/home";
+import Shopping from "./components/shopping";
+import Cart from "./components/cart";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/shopping" exact component={Shopping} />
+        <Route path="/cart" exact component={Cart} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/shopping.jsx
@@ -0,0 +1,9 @@
+const Shopping = () => {
+  return (
+    <div>
+      <h1>Hello from Shopping</h1>
+    </div>
+  );
+};
+
+export default Shopping;
